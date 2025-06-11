919397482ac7794bbed7897d02637120fb02d7ee
JCharly95 <juancarloscharly@live.com.mx>
2025-06-10 23:41:20 -0600

    Instalacion y configuracion de Inertia.js
    En este commit se instalaron las dependencias necesarias para hacer funcionar Inertia de forma satisfactoria. Asi como, las respectivas configuraciones para que se pueda enlazar y operar de forma adecuada e incluso se creo una pagina/componente react para demostrar su funcionamiento.

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return <>
+        <h1 className="text-3xl font-bold underline text-gray-400">
+            Hello world!
+        </h1>
+    </>
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+/*export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,4 +11,19 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+});*/
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@' : "/resources/js",
+        },
+    },
 });
