796d45e92ec31bc838335b3a96f914f2c6bc8359
JCharly95 <juancarloscharly@live.com.mx>
2025-08-25 12:35:59 -0600

    Separacion chunks front y agregado de css al build del front
    En este commit se agregó un bloque de configuración en el archivo de vite para al momento de generar el build del front  lo separe por chunks mas pequeños para poderlos procesar mejor. También se agregó el archivo css de la app tanto en la configuración de vite como en la app blade de la aplicación.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,10 +8,9 @@
         <meta name="description" content="El mejor sistema web para gestionar tu inmueble inteligente" />
         <link rel="icon" type="image/x-icon" href="/images/Icono_Compacto.png">
         @viteReactRefresh
-        @vite('resources/js/app.jsx')
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
     </head>
-    <!--<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">-->
     <body>
         @inertia
     </body>
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-/*export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});*/
 export default defineConfig({
     plugins: [
         laravel({
@@ -26,6 +17,27 @@ export default defineConfig({
             '@' : "/resources/js",
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id){
+                    if (id.includes('node_modules')) {
+                        const modulePath = id.split('node_modules/')[1];
+                        const topLevelFolder = modulePath?.split('/')[0];
+                        if (topLevelFolder !== '.pnpm') {
+                            return topLevelFolder;
+                        }
+                        
+                        // changed . to ?. for the two lines below:
+                        const scopedPackageName = modulePath?.split('/')[1];
+                        const chunkName = scopedPackageName?.split('@')[scopedPackageName.startsWith('@') ? 1 : 0];
+
+                        return chunkName;
+                    }
+                }
+            }
+        }
+    }
     /*server: {
         host: '101.40.4.131', // Establecer la dirección IP desde donde el servidor (php artisan) entragará el contenido del sitio
         cors: true, // Allow all origins for local development
